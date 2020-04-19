07e4d660ea1a9f89ae706ccf99356944385e9ff9
jeanfilho <jeanfilho@gmail.com>
2020-04-19 15:59:19 +0200

    added missing commit

--- CloudRendering-Vulkan/RenderTechniquePPM.cpp
+++ CloudRendering-Vulkan/RenderTechniquePPM.cpp
@@ -121,6 +121,11 @@ void RenderTechniquePPM::FreePhotonMap()
 
 void RenderTechniquePPM::AllocatePhotonMap(VulkanBuffer* photonMapPropertiesBuffer)
 {
+	if (m_photonMap)
+	{
+		FreePhotonMap();
+	}
+
 	uint32_t bufferSize = m_photonMapProperties->GetTotalSize();
 
 	VkBufferUsageFlags flags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
